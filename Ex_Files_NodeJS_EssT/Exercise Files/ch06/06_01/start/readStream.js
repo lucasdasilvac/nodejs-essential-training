6f2e37d1627fba5fe1bcc65c4369ef0d39898e33
lucasdasilvac <lucascosta@alu.ufc.br>
2019-09-17 13:15:48 -0300

    chapter 6 done

--- Ex_Files_NodeJS_EssT/Exercise Files/ch06/06_01/start/readStream.js
+++ Ex_Files_NodeJS_EssT/Exercise Files/ch06/06_01/start/readStream.js
@@ -1,4 +1,12 @@
+const fs = require("fs");
+
+const readStream = fs.createReadStream("./assets/lorum-ipsum.md", "UTF-8");
+
 console.log("type something...");
 process.stdin.on("data", data => {
   console.log(`I read ${data.length - 1} characters of text`);
+});
+
+readStream.once("data", data =>{
+  console.log(data);
 });
